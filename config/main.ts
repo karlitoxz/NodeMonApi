2cebb8720c1895780864c252ca76efd7660c2440
Juan <satelimportadores@gmail.com>
2020-09-09 15:34:40 -0500

    Rutas

--- config/main.ts
+++ config/main.ts
@@ -1,3 +1,12 @@
-{
-    
-}
+const config = {
+    //Puerto
+        port: process.env.PORT || 3000,
+    //Database
+        db: 'mongodb://localhost/dbnodemonapi',
+    //test enviroment
+        test_env: 'test',
+        test_db: 'mongodb://localhost/dbnodemonapi-test',
+        test_port: 3001
+
+};
+export default config;
